4975db90264d39481781c57ebe8bcdda06d42107
michelelanotte <lanotte28@gmail.com>
2022-01-07 15:33:25 +0100

    new web interface and introduction of class ViewSOPInfo to manage the data to show on the web app

--- dash_Plotly_server.py
+++ dash_Plotly_server.py
@@ -1,64 +1,68 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jan  7 12:04:04 2022
+
+@author: Utente
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Nov 18 11:18:56 2021
 
 @author: Utente
 """
 
 from utility.utils import pd, setMidpointAndBBox, getDataframeByCity, findRowByCoordinate
+from utility.viewSOPInfo import ViewSOPInfo
 import plotly.express as px
 
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html, callback_context
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
-global df, dataset
-
-#Dictionary which will contain the midpoints of the cities indicate as keys.
-#the elements will be in the form: <lat, lon>
-global dict_coords_midpoint
-
-#Dictionary which will contain the bbox of the cities indicate as keys
-#the elements will be in the form: <<lat_min, lat_max>, <lon_min, lon_max>>
-global dict_coords_bbox
-
-app = dash.Dash("Sentiment On Place Twitter", external_stylesheets=[dbc.themes.SPACELAB])
+app = dash.Dash("Sentiment On Place Twitter")
+view_info = ViewSOPInfo()
 
 
-def view_page(title, fig_map, fig_pie):
-    nav_contents = [
-        dbc.NavItem(dbc.NavLink("Wellington", active=True)),
-        dbc.NavItem(dbc.NavLink("San Francisco", href="")),
-        dbc.NavItem(dbc.NavLink("New York", href="")),
-        dbc.NavItem(dbc.NavLink("Sydney", href="")),
-        dbc.NavItem(dbc.NavLink("London", href="")),
-    ]
-    nav = dbc.Nav(nav_contents, pills=True, fill=True)
+def view_page():
+    buttons = html.Div(
+        [
+            dbc.Button("Wellington", id="we", outline=True, color="info", className="me-1"),
+            dbc.Button("San Francisco", id="sf", outline=True, color="info", className="me-1"),
+            dbc.Button("New York", id="ny", outline=True, color="info", className="me-1"),
+            dbc.Button("Sydney", id="sy", outline=True, color="info", className="me-1"),
+            dbc.Button("London", id="lo", outline=True, color="info", className="me-1"),
+        ]
+    )
     
     app.layout = html.Div([
         html.Div([
             html.H1("Sentiment On Place Twitter", style={'textAlign': 'center', 'fontSize': 32, 'fontWeight': 'bold'})]
         ),
+        html.Hr(),
         html.Div([
-            dbc.Container([nav], fluid=True)]
+            dbc.Container([buttons])],
+            style={
+                'textAlign': 'center'
+            }
         ),
         html.Hr(),
         html.Div([
-            html.H1(title, id="title", style={'textAlign': 'center', 'fontSize': 24, 'marginTop': '50px'})]
+            html.H1(view_info.get_title(), id="title", style={'textAlign': 'center', 'fontSize': 24, 'marginTop': '50px'})]
         ),
         html.Div([
-            dcc.Graph(id="bbox", figure=fig_map)
+            dcc.Graph(id="bbox", figure=view_info.get_fig_map())
             ],
             style={
                 "display": "inline-block",
-                "width": "70%"
+                "width": "70%",
+                "marginLeft": "50px",            
             }
         ),
         html.Div([
             html.Div([
-                dcc.Graph(id="filter-percentage-sentiments", figure=fig_pie)
+                dcc.Graph(id="filter-percentage-sentiments", figure=view_info.get_fig_pie())
             ]),
         ], 
         style={
@@ -69,7 +73,7 @@ def view_page(title, fig_map, fig_pie):
         }),
         html.Div([
                 html.H1("Selected area", style={'textAlign': 'center', 'fontSize': 32}),
-                dcc.Graph(id="selected-area", figure=fig_map, config={'displayModeBar': False})
+                dcc.Graph(id="selected-area", figure=view_info.get_fig_selected_area(), config={'displayModeBar': False})
             ],
             style={
                 "width": "70%",
@@ -78,10 +82,7 @@ def view_page(title, fig_map, fig_pie):
                 "marginRight": "auto",
                 "borderStyle": "solid",
                 "borderColor": "black"
-        }),
-        html.Div([
-            html.H1(id="pippo", style={'textAlign': 'center', 'fontSize': 24, 'marginTop': '50px'})]
-        ),
+        })
     ])   
 
 
@@ -90,21 +91,22 @@ This method, starting from the input city, builds the map with the tweets of tha
 with the percentages of positive tweets and negative.
 """
 def create_map_bokeh(city):
-    list_of_globals = globals()
-    midpoint_lat = list_of_globals["dict_coords_midpoint"][city][0]
-    midpoint_long = list_of_globals["dict_coords_midpoint"][city][1]
+    df = view_info.get_df_selected_city()
+    coords = view_info.get_dict_coords_midpoint()[city]
+    midpoint_lat = coords[0]
+    midpoint_long = coords[1]
     
     #Size of points in the map
-    size = list_of_globals["df"].shape[0]
-    list_of_globals["df"]["Size"] = [5 for i in range(size)]
+    size = df.shape[0]
+    df["Size"] = [5 for i in range(size)]
+    view_info.set_df_selected_city(df)
     
-    fig_map = set_bbox(list_of_globals["df"], midpoint_lat, midpoint_long)
+    fig_map = set_bbox(df, midpoint_lat, midpoint_long)
        
     #plot percentuale sentiment positivi e negativi
-    fig_pie = px.pie(list_of_globals["df"], names="Sentiment", color='Sentiment', height=350)
+    fig_pie = px.pie(df, names="Sentiment", color='Sentiment', height=350)
     fig_pie.update_traces(hovertemplate=None, title="Sentiment percentage", showlegend=False)
-    
-    
+     
     return fig_map, fig_pie
 
     
@@ -112,7 +114,7 @@ def set_bbox(source, midpoint_lat, midpoint_long):
     token = "pk.eyJ1IjoibWxhbm90dGUxNSIsImEiOiJja3c0dmR6d3MwODhmMnVyaDE2aHpxMG11In0.Z9815N56yl0BfjRIiaFUkA"
     px.set_mapbox_access_token(token)
     fig_map = px.scatter_mapbox(source, hover_name="Tweet", lat="Lat", lon="Lon",
-                            color="Sentiment", zoom=11, size="Size", size_max=10,
+                            color="Sentiment", zoom=12, size="Size", size_max=10,
                             center={"lat": midpoint_lat, "lon": midpoint_long})
     #fig_map.update_layout(mapbox_style="open-street-map")
     fig_map.update_layout(margin={"r":10,"t":20,"l":10,"b":1})
@@ -130,70 +132,94 @@ def set_bbox(source, midpoint_lat, midpoint_long):
     Input('bbox', 'selectedData')  #acquisition of the points included in the selected box or in the selected period
 )
 def display_selected_data(selectedData):
-    fig_pie = {}
-    fig_selected_area = {}
-    points = selectedData["points"] 
-    
-    data = []
-    for i in range(len(points)):
-        lat = points[i]['lat']
-        lon = points[i]['lon']
-        coords = (lat, lon)
+    fig_pie = view_info.get_fig_pie()
+    fig_selected_area = view_info.get_fig_selected_area()
+    if selectedData:
+        df = view_info.get_df_selected_city()
+        points = selectedData["points"] 
         
-        row = findRowByCoordinate(list_of_globals["df"], coords)
-        if row is not None:
-            data.append(row)
-    
-    selected_df = pd.DataFrame(data, columns = ["Tweet", "Sentiment", "Lat", "Lon", "Size"])
-    fig_pie = px.pie(selected_df, names = 'Sentiment', color = 'Sentiment')
-    fig_pie.update_traces(hovertemplate = None, title = "Sentiment percentage", showlegend = False)  
-    
-    
-    """********************Display of the coordinates selected in the bbox**************************"""    
-    fig_selected_area = px.scatter_mapbox(selected_df, lat="Lat", lon="Lon", hover_name="Tweet",
-                            color="Sentiment", size="Size", zoom=14, size_max=10) 
-    fig_selected_area.update_layout(margin={"r":10,"t":20,"l":10,"b":1})
-    fig_selected_area.update_layout(legend=dict(
-        title = "Colors for sentiments:", orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
-    ))
-    fig_selected_area.update_traces(mode="markers", hovertemplate=None) 
+        data = []
+        for i in range(len(points)):
+            lat = points[i]['lat']
+            lon = points[i]['lon']
+            coords = (lat, lon)
+            
+            row = findRowByCoordinate(df, coords)
+            if row is not None:
+                data.append(row)
+        
+        selected_df = pd.DataFrame(data, columns = ["Tweet", "Sentiment", "Lat", "Lon", "Size"])
+        fig_pie = px.pie(selected_df, names = 'Sentiment', color = 'Sentiment')
+        fig_pie.update_traces(hovertemplate = None, title = "Sentiment percentage", showlegend = False)  
+        
+        
+        """********************Display of the coordinates selected in the bbox**************************"""    
+        fig_selected_area = px.scatter_mapbox(selected_df, lat="Lat", lon="Lon", hover_name="Tweet",
+                                color="Sentiment", size="Size", zoom=14, size_max=10) 
+        fig_selected_area.update_layout(margin={"r":10,"t":20,"l":10,"b":1})
+        fig_selected_area.update_layout(legend=dict(
+            title = "Colors for sentiments:", orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
+        ))
+        fig_selected_area.update_traces(mode="markers", hovertemplate=None) 
     
     return fig_pie, fig_selected_area
     
 
-
-
-
-#TO DO**********************************
-"""@app.callback(
-    #Output('pippo', 'children'), 
-    Input('select-city', 'value')
+@app.callback(
+    Output('title', 'children'), 
+    Output('bbox', 'figure'),
+    Input('we', 'n_clicks'), 
+    Input('ny', 'n_clicks'), 
+    Input('sy', 'n_clicks'), 
+    Input('sf', 'n_clicks'), 
+    Input('lo', 'n_clicks')
 )
-def display_city_map(city):
-    list_of_globals = globals()
-    list_of_globals['df'] = getDataframeByCity(city, list_of_globals["dataset"], list_of_globals["dict_coords_bbox"])
+def display_city_map(we, ny, sy, sf, lo):  
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'we' in changed_id:
+        city = "wellington"
+    elif 'ny' in changed_id:
+        city = "new york"
+    elif 'sy' in changed_id:
+        city = "sydney"
+    elif 'sf' in changed_id:
+        city = "san francisco"
+    elif 'lo' in changed_id:
+        city = "london"
+    
+    df = getDataframeByCity(city, view_info.get_dataset(), view_info.get_dict_coords_bbox())
+    view_info.set_df_selected_city(df)
     fig_map, fig_pie = create_map_bokeh(city) 
-    return {}.format(value)"""
+    title = "Plotting Sentiments for " + city.capitalize() + " city"
+    return title, fig_map
 
   
-"""********************************** MAIN **********************************"""
-locality = ["wellington", "new york", "san francisco", "sydney", "london"]
-
-#get all global variables
-list_of_globals = globals()
-list_of_globals["dict_coords_midpoint"], list_of_globals["dict_coords_bbox"] = setMidpointAndBBox(locality)
-
-filename = "dataset/sentiments_and_coords.tsv" 
-list_of_globals["dataset"] = pd.read_csv(filename, sep = '\t', encoding = "utf-8", header = 0)
-
-#The Wellington map is shown by default at system launch
-city = locality[3]
-list_of_globals["df"] = getDataframeByCity(city, list_of_globals["dataset"], list_of_globals["dict_coords_bbox"])  
-fig_map, fig_pie = create_map_bokeh(city) 
-title = "Plotting Sentiments for " + city.capitalize() + " city"
-view_page(title, fig_map, fig_pie)
+def setViewInfo():
+    locality = view_info.get_cities()
     
+    dict_coords_midpoint, dict_coords_bbox = setMidpointAndBBox(locality)
+    view_info.set_dict_coords_midpoint(dict_coords_midpoint)
+    view_info.set_dict_coords_bbox(dict_coords_bbox)
     
+    filename = "dataset/sentiments_and_coords.tsv" 
+    view_info.set_dataset(pd.read_csv(filename, sep = '\t', encoding = "utf-8", header = 0))
+    
+    #The Wellington map is shown by default at system launch
+    city = locality[0]
+    #get tweets found in city area
+    view_info.set_df_selected_city(getDataframeByCity(city, view_info.get_dataset(), view_info.get_dict_coords_bbox()))
+    fig_map, fig_pie = create_map_bokeh(city) 
+    view_info.set_fig_map(fig_map)
+    view_info.set_fig_selected_area(fig_map)
+    view_info.set_fig_pie(fig_pie)
+    view_info.set_title("Plotting Sentiments for " + city.capitalize() + " city")
+    
+    return view_info
+    
+
+"""********************************START SERVER********************************"""
+view_info = setViewInfo()    
 if __name__ == '__main__':
+    view_page()
     app.run_server(debug=True)
     #app.run_server(debug=False,dev_tools_ui=False,dev_tools_props_check=False)
---
+++ utility/viewSOPInfo.py
@@ -0,0 +1,69 @@
+class ViewSOPInfo:
+    def __init__(self):
+        self.__cities = ["wellington", "new york", "san francisco", "sydney", "london"]
+        self.__dataset = None
+        self.__df_selected_city = None
+        
+        #Dictionary which will contain the midpoints of the cities indicate as keys.
+        #the elements will be in the form: <lat, lon>
+        self.__dict_coords_midpoint = None
+        
+        #Dictionary which will contain the bbox of the cities indicate as keys
+        #the elements will be in the form: <<lat_min, lat_max>, <lon_min, lon_max>>
+        self.__dict_coords_bbox = None
+        self.__fig_map = None
+        self.__fig_pie = None
+        self.__fig_selected_area = None
+        self.__title = ""
+        
+    def set_dataset(self, dataset):
+            self.__dataset = dataset
+    
+    def get_dataset(self):
+            return self.__dataset
+    
+    def set_df_selected_city(self, df):
+            self.__df_selected_city = df
+    
+    def get_df_selected_city(self):
+            return self.__df_selected_city
+
+    def set_dict_coords_midpoint(self, dict_coords_midpoint):
+            self.__dict_coords_midpoint = dict_coords_midpoint
+    
+    def get_dict_coords_midpoint(self):
+            return self.__dict_coords_midpoint
+
+    def set_dict_coords_bbox(self, dict_coords_bbox):
+            self.__dict_coords_bbox = dict_coords_bbox
+    
+    def get_dict_coords_bbox(self):
+            return self.__dict_coords_bbox
+    
+    def set_fig_map(self, fig_map):
+        self.__fig_map = fig_map
+    
+    def get_fig_map(self):
+        return self.__fig_map
+    
+    def set_fig_pie(self, fig_pie):
+        self.__fig_pie = fig_pie
+    
+    def get_fig_pie(self):
+        return self.__fig_pie
+    
+    def set_fig_selected_area(self, fig_selected_area):
+        self.__fig_selected_area = fig_selected_area
+    
+    def get_fig_selected_area(self):
+        return self.__fig_selected_area
+
+    def set_title(self, title):
+        self.__title = title
+          
+    def get_title(self):
+        return self.__title
+    
+    def get_cities(self):
+        return self.__cities
+        
