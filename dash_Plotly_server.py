b1cbdd0aa0692b08c8392177d81f953650e777c4
michelelanotte <lanotte28@gmail.com>
2022-01-08 13:39:01 +0100

    fix style web app

--- dash_Plotly_server.py
+++ dash_Plotly_server.py
@@ -22,67 +22,59 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 app = dash.Dash("Sentiment On Place Twitter", external_stylesheets=[dbc.themes.SPACELAB])
+#app = dash.Dash("Sentiment On Place Twitter", external_stylesheets=[dbc.themes.SPACELAB])
 view_info = ViewSOPInfo()
 
 
 def view_page():
     buttons = html.Div(
         [
-            dbc.Button("Wellington", id="we", outline=True, color="info", className="me-1"),
-            dbc.Button("San Francisco", id="sf", outline=True, color="info", className="me-1"),
-            dbc.Button("New York", id="ny", outline=True, color="info", className="me-1"),
-            dbc.Button("Sydney", id="sy", outline=True, color="info", className="me-1"),
-            dbc.Button("London", id="lo", outline=True, color="info", className="me-1"),
-        ]
+            dbc.Button("Wellington", id="city0", outline=True, color="info", className="me-1 btn-menu"),
+            dbc.Button("San Francisco", id="city1", outline=True, color="info", className="me-1 btn-menu"),
+            dbc.Button("New York", id="city2", outline=True, color="info", className="me-1 btn-menu"),
+            dbc.Button("Sydney", id="city3", outline=True, color="info", className="me-1 btn-menu"),
+            dbc.Button("London", id="city4", outline=True, color="info", className="me-1 btn-menu"),
+        ],
+        style={'whiteSpace': 'nowrap'}
     )
     
     app.layout = html.Div([
         html.Div([
-            html.H1("Sentiment On Place Twitter", style={'textAlign': 'center', 'fontSize': 32, 'fontWeight': 'bold'})]
+            html.H1("Sentiment On Place Twitter", className="title")]
         ),
         html.Hr(),
         html.Div([
-            dbc.Container([buttons])],
+            dbc.NavbarBrand(buttons),
+            ],
             style={
                 'textAlign': 'center'
             }
         ),
-        html.Hr(),
+        html.Hr(style={"marginTop": "-10px"}),
         html.Div([
-            html.H1(view_info.get_title(), id="title", style={'textAlign': 'center', 'fontSize': 24, 'marginTop': '50px'})]
-        ),
-        html.Div([
-            dcc.Graph(id="bbox", figure=view_info.get_fig_map())
-            ],
-            style={
-                "display": "inline-block",
-                "width": "70%",
-                "marginLeft": "50px",            
-            }
-        ),
+            html.H1(view_info.get_title(), id="title", className="subtitle")
+        ]),
         html.Div([
             html.Div([
-                dcc.Graph(id="filter-percentage-sentiments", figure=view_info.get_fig_pie())
-            ]),
-        ], 
-        style={
-            "display": "inline-block",
-            "width": "25%",
-            "marginLeft": "5px",
-            "verticalAlign": "top",
-        }),
+                dcc.Graph(id="bbox", figure=view_info.get_fig_map())
+            ], className="bbox-div"
+            ),
+            html.Div([
+                dcc.Graph(id="filter-percentage-sentiments", figure=view_info.get_fig_pie(), 
+                          config={'displayModeBar': False}, style={"textAlign": "center"})
+                ],
+                style={
+                    "display": "inline-block",
+                    "width": "25%",
+                    "marginLeft": "10px",
+                    "verticalAlign": "top",
+                })
+        ]),
         html.Div([
-                html.H1("Selected area", style={'textAlign': 'center', 'fontSize': 32}),
+                html.H1("Selected area", className="subtitle"),
                 dcc.Graph(id="selected-area", figure=view_info.get_fig_selected_area(), config={'displayModeBar': False})
-            ],
-            style={
-                "width": "70%",
-                "marginTop": "5%",
-                "marginLeft": "auto",
-                "marginRight": "auto",
-                "borderStyle": "solid",
-                "borderColor": "black"
-        })
+            ], className="selected-area-div"
+        )
     ])   
 
 
@@ -104,8 +96,15 @@ def create_map_bokeh(city):
     fig_map = set_bbox(df, midpoint_lat, midpoint_long)
        
     #plot percentuale sentiment positivi e negativi
-    fig_pie = px.pie(df, names="Sentiment", color='Sentiment', height=350)
-    fig_pie.update_traces(hovertemplate=None, title="Sentiment percentage", showlegend=False)
+    fig_pie = px.pie(df, names="Sentiment", color='Sentiment', height=380)
+    fig_pie.update_traces(hovertemplate=None, showlegend=False)
+    fig_pie.update_layout(title={
+        'text' : 'Sentiments percentage',
+        'x':0.5,
+        'y':0.9,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    })
      
     return fig_map, fig_pie
 
@@ -122,7 +121,6 @@ def set_bbox(source, midpoint_lat, midpoint_long):
         title="Colors for sentiments:", orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
     ))
     fig_map.update_traces(mode="markers", hovertemplate=None) 
-    
     return fig_map
 
 
@@ -137,30 +135,36 @@ def display_selected_data(selectedData):
     if selectedData:
         df = view_info.get_df_selected_city()
         points = selectedData["points"] 
-        
-        data = []
-        for i in range(len(points)):
-            lat = points[i]['lat']
-            lon = points[i]['lon']
-            coords = (lat, lon)
+        if len(points) > 1:
+            data = []
+            for i in range(len(points)):
+                lat = points[i]['lat']
+                lon = points[i]['lon']
+                coords = (lat, lon)
+                
+                row = findRowByCoordinate(df, coords)
+                if row is not None:
+                    data.append(row)
             
-            row = findRowByCoordinate(df, coords)
-            if row is not None:
-                data.append(row)
-        
-        selected_df = pd.DataFrame(data, columns = ["Tweet", "Sentiment", "Lat", "Lon", "Size"])
-        fig_pie = px.pie(selected_df, names = 'Sentiment', color = 'Sentiment')
-        fig_pie.update_traces(hovertemplate = None, title = "Sentiment percentage", showlegend = False)  
-        
-        
-        """********************Display of the coordinates selected in the bbox**************************"""    
-        fig_selected_area = px.scatter_mapbox(selected_df, lat="Lat", lon="Lon", hover_name="Tweet",
-                                color="Sentiment", size="Size", zoom=14, size_max=10) 
-        fig_selected_area.update_layout(margin={"r":10,"t":20,"l":10,"b":1})
-        fig_selected_area.update_layout(legend=dict(
-            title = "Colors for sentiments:", orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
-        ))
-        fig_selected_area.update_traces(mode="markers", hovertemplate=None) 
+            selected_df = pd.DataFrame(data, columns = ["Tweet", "Sentiment", "Lat", "Lon", "Size"])
+            fig_pie = px.pie(selected_df, names = 'Sentiment', color = 'Sentiment')
+            fig_pie.update_traces(hovertemplate = None, showlegend = False)  
+            fig_pie.update_layout(title={
+                'text' : 'Sentiments percentage',
+                'x':0.5,
+                'y':0.9,
+                'xanchor': 'center',
+                'yanchor': 'top'
+            })
+              
+            """********************Display of the coordinates selected in the bbox**************************"""    
+            fig_selected_area = px.scatter_mapbox(selected_df, lat="Lat", lon="Lon", hover_name="Tweet",
+                                    color="Sentiment", size="Size", zoom=14, size_max=10) 
+            fig_selected_area.update_layout(margin={"r":10,"t":20,"l":10,"b":1})
+            fig_selected_area.update_layout(legend=dict(
+                title = "Colors for sentiments:", orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
+            ))
+            fig_selected_area.update_traces(mode="markers", hovertemplate=None) 
     
     return fig_pie, fig_selected_area
     
@@ -168,29 +172,23 @@ def display_selected_data(selectedData):
 @app.callback(
     Output('title', 'children'), 
     Output('bbox', 'figure'),
-    Input('we', 'n_clicks'), 
-    Input('ny', 'n_clicks'), 
-    Input('sy', 'n_clicks'), 
-    Input('sf', 'n_clicks'), 
-    Input('lo', 'n_clicks')
+    [Input(f'city{i}', 'n_clicks') for i in range(0, 5)]
 )
 def display_city_map(we, ny, sy, sf, lo):  
     changed_id = [p['prop_id'] for p in callback_context.triggered][0]
-    if 'we' in changed_id:
-        city = "wellington"
-    elif 'ny' in changed_id:
-        city = "new york"
-    elif 'sy' in changed_id:
-        city = "sydney"
-    elif 'sf' in changed_id:
-        city = "san francisco"
-    elif 'lo' in changed_id:
-        city = "london"
+    for i in range(0, 5):
+        if "city"+str(i) in changed_id:
+            city = view_info.get_city_by_index(i)
+            break
     
     df = getDataframeByCity(city, view_info.get_dataset(), view_info.get_dict_coords_bbox())
     view_info.set_df_selected_city(df)
     fig_map, fig_pie = create_map_bokeh(city) 
+    view_info.set_fig_map(fig_map)
+    view_info.set_fig_selected_area(fig_map)
+    view_info.set_fig_pie(fig_pie)
     title = "Plotting Sentiments for " + city.capitalize() + " city"
+    view_info.set_title(title)
     return title, fig_map
 
   
